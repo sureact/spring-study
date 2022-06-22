70fe1fc62d05bc5b19934e0096d15fc00f2ca6e2
milk717 <robolindasoo@gmail.com>
2022-06-22 17:18:50 +0900

    update: hello-api, ResponseBody

--- hello-spring/src/main/java/org/strongcom/study/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/org/strongcom/study/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,9 +15,39 @@ public class HelloController {
         return "hello";
     }
 
+    //쿼리문으로 name 파라미터 요청가능
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model ){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    //html 없이 문자 그대로 내려감.
+    //쓸일 거의 없긴함.
+    @GetMapping("hello-string")
+    @ResponseBody   //http body 에 데이터를 직접 넣어준다.
+    public String helloString(@RequestParam("name")String name){
+        return "hello "+name;
+    }
+
+    // 객체를 내려주면 json 방식으로 표현됨
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
