c916b487e7dbd27d574d3f47320ea2d1e7910916
milk717 <robolindasoo@gmail.com>
2022-06-22 16:39:04 +0900

    update: hello-mvc, RequestParam

--- hello-spring/src/main/java/org/strongcom/study/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/org/strongcom/study/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package org.strongcom.study.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
         //key == data, value == hello
         model.addAttribute("data","hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model ){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
