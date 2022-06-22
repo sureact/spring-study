1dc4235be5f6396116d07c6287b98c5190af7882
milk717 <robolindasoo@gmail.com>
2022-06-22 18:48:51 +0900

    update: MemberService.java

---
+++ hello-spring/src/main/java/org/strongcom/study/hellospring/service/MemberService.java
@@ -0,0 +1,41 @@
+package org.strongcom.study.hellospring.service;
+
+import org.strongcom.study.hellospring.domain.Member;
+import org.strongcom.study.hellospring.repository.MemberRepository;
+import org.strongcom.study.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        //이름이 같은 회원이 존재 x
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
