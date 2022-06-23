97f3807c0f396ee0cab6833b2b1af6aa4a0e1481
milk717 <robolindasoo@gmail.com>
2022-06-23 19:53:44 +0900

    feat: db

--- java-practice/src/Stream.java
+++ java-practice/src/Stream.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 public class Stream {
     public static void main(String[] args) {
         ArrayList<User> userArrayList = new ArrayList<User>();
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("Apple","Banana","Melon","Grape","Strawberry"));
 
         userArrayList.add(new User(0L, "sumin"));
         userArrayList.add(new User(1L, "minsu"));
@@ -15,8 +14,5 @@ public class Stream {
                 .filter(name -> name.contains("u"))
                 .forEach(System.out::println);
 
-        list.stream()
-                .filter(t->t.contains("a"))
-                .forEach(System.out::println);
     }
 }
